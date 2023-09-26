8c5c87b7ab1af34242f0ae0b50caad4986f14093
roylee0704 <roylee0704@gmail.com>
2023-09-26 15:49:15 +0700

    feat: loading.tsx

---
+++ next01/app/about/loading.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+// note(roy): automatically used by Next.js, it uses React Suspense
+// under the hood.
+export default function Loading() {
+  return (
+    <h1>Loading....</h1>
+  )
+}
