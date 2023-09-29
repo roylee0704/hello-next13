5e9b17fdebb9a106ac65d3c7d9a0212331f991aa
roylee0704 <roylee0704@gmail.com>
2023-09-29 15:56:40 +0700

    feat: search component

---
+++ next05-client-component/app/[searchTerm]/loading.tsx
@@ -0,0 +1,11 @@
+// note(roy): applies to entire route now, not part of the page
+// note(roy): show loading due to react suspense.
+import React from "react";
+
+export default function loading() {
+  return (
+    <main>
+      <h2>Loading...</h2>
+    </main>
+  );
+}
--- next05-client-component/app/components/Navbar.tsx
+++ next05-client-component/app/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import React from "react";
+import Search from "./Search";
 
 export default function Navbar() {
   return (
-    <nav className="flex justify-between bg-slate-600 p-4 flex-col">
-      <h1>
+    <nav className="flex justify-between flex-col md:flex-row sticky top-0 drop-shadow-xl bg-slate-600 p-4">
+      <h1 className="text-3xl font-bold text-white grid place-content-center mb-2 md:mb-0">
         <Link href="/">WikiRocket!</Link>
       </h1>
+      <Search />
     </nav>
   );
 }
