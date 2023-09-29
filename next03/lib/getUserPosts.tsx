b26b10b463dd5191584cfdc7a00fed673386d2c4
roylee0704 <roylee0704@gmail.com>
2023-09-29 10:33:38 +0700

    feat: SSG + not found

--- next03/lib/getUserPosts.tsx
+++ next03/lib/getUserPosts.tsx
@@ -1,5 +1,5 @@
 export default async function getUserPosts(userId: string) {
     const res = await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`);
-    if (!res.ok) throw new Error("failed to fetch user posts");
+    if (!res.ok) return undefined;
     return res.json();
   }
